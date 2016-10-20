317c636fac70155c183bf96eb124c757716a1e9b
Teidata <spa@teidata.com>
2016-10-20 19:20:05 +0100

    Update panorama.js

--- panorama.js
+++ panorama.js
@@ -1,5 +1,10 @@
 $(document).ready(function() {
 
- 
+// Javascript
+var PSV = PhotoSphereViewer({
+  panorama:  'URL_DE_LA_IMAGEN',
+  container: 'ID_DEL_DIV_DONDE_IRA_LA_IMAGEN',
+  loading_img: 'https://media.giphy.com/media/TtZqlvHid7BjW/giphy.gif',
+});
     
 });
