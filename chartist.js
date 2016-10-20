ec1f82f0bfa58d796f6b3cf216bcf08ae5622d7b
Teidata <spa@teidata.com>
2016-10-20 16:28:06 +0100

    Update chartist.js

--- chartist.js
+++ chartist.js
@@ -9,7 +9,7 @@ var data = {
 var options = {}
 
 new Chartist.Line('.chart', data, options);
-});
+
 var data2 = {
   labels: ['A1', 'A2', 'A3', 'A4', 'A5', 'A6'],
   series: [{ data: [2, 4, 6, 8, 10, 12] }, // ct-series-a
@@ -21,3 +21,5 @@ var data2 = {
 var options2 = {}
 
 new Chartist.Line('.chart2', data2, options2);
+  
+});
