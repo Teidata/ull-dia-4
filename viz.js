3d62b8b117b7cbc9335a1cec002d1d4ba7743bf9
Teidata <spa@teidata.com>
2016-10-20 17:23:52 +0100

    Update viz.js

--- viz.js
+++ viz.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-var sample = 'digraph g { a -> b [label="Etiqueta 1"]; b -> a [style=dashed]; a [style=bold]; b [style=red];}';
+var sample = 'digraph g { antonio -> b [label="Etiqueta 1"]; a -> b [label="Etiqueta 1"]; b -> a [style=dashed]; a [style=bold]; b [style=red];}';
 
 var options = {
   format: 'svg'
