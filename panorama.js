5862a7c456ab7a7194f58da68a6de6f4ba17f566
Teidata <spa@teidata.com>
2016-10-20 19:30:20 +0100

    Update panorama.js

--- panorama.js
+++ panorama.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 
 // Javascript
 var PSV = PhotoSphereViewer({
-  panorama:  'URL_DE_LA_IMAGEN',
+  panorama:  'https://teidata.github.io/ull-dia-4/panorama-01.jpg',
   container: 'ID_DEL_DIV_DONDE_IRA_LA_IMAGEN',
   loading_img: 'https://media.giphy.com/media/TtZqlvHid7BjW/giphy.gif',
 });
