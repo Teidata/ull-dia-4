391bf0ffda73459194e17004b1764de2a77b71aa
Teidata <spa@teidata.com>
2016-10-20 16:56:17 +0100

    Update sequence.js

--- sequence.js
+++ sequence.js
@@ -1,10 +1,7 @@
 $(document).ready(function() {
 
-var sample1 = 'Title: Esto es un título';
-var sample2 = 'B-->C: Línea a rayas';
-var sample3 = 'C->>D: Línea con flecha hueca';
-var sample4 = 'D-->>E: Línea a rayas con flecha hueca';
-var sample5 = 'A->C: Uno \n B->C: Dos';
+var sample = 'A->B: Mensaje';
+
 
 var diagram = Diagram.parse(sample);
 
